2802755ae5f1a8e5dc1623809ac7f83a4dd462d5
Greg Cherry <gcherry2@gmail.com>
2014-04-12 22:48:00 -0400

    Manager

---
+++ LOLAPI.Logic/RiotAPIManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOLAPI.Logic
+{
+    public class RiotAPIManager
+    {
+
+        protected static readonly string gncRiotAPIKey;
+
+        static RiotAPIManager() 
+        {
+            gncRiotAPIKey = "c92b6d81-20ff-438c-a966-503fa839c3e5";
+        }
+    }
+}
